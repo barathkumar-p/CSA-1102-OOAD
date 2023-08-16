4be1c44c46033ea55f16446c907dbe1e2d512aaf
barathkumar-p <81065407+barathkumar-p@users.noreply.github.com>
2023-08-16 10:31:23 +0530

    Add files via upload

---
+++ EX-3 EXAM REGISTRATION SYSTEM/CODE/university.java
@@ -0,0 +1,88 @@
+
+import java.util.*;
+
+
+/**
+ * Class university
+ */
+public class university {
+
+  //
+  // Fields
+  //
+
+  private void student_details;
+  private void exam_details;
+  
+  //
+  // Constructors
+  //
+  public university () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of student_details
+   * @param newVar the new value of student_details
+   */
+  private void setStudent_details (void newVar) {
+    student_details = newVar;
+  }
+
+  /**
+   * Get the value of student_details
+   * @return the value of student_details
+   */
+  private void getStudent_details () {
+    return student_details;
+  }
+
+  /**
+   * Set the value of exam_details
+   * @param newVar the new value of exam_details
+   */
+  private void setExam_details (void newVar) {
+    exam_details = newVar;
+  }
+
+  /**
+   * Get the value of exam_details
+   * @return the value of exam_details
+   */
+  private void getExam_details () {
+    return exam_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void maintainRecord()
+  {
+  }
+
+
+  /**
+   */
+  public void verify_student()
+  {
+  }
+
+
+  /**
+   */
+  public void approve_student()
+  {
+  }
+
+
+}
