1db38e7dd3c96dac673b9415a59e2eb4fe122665
barathkumar-p <81065407+barathkumar-p@users.noreply.github.com>
2023-08-17 10:08:04 +0530

    Add files via upload

---
+++ EX-12 FOREIGN TRADING MANAGEMENT SYSTEM/CODE/SUPLIER.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class SUPLIER
+ */
+public class SUPLIER {
+
+  //
+  // Fields
+  //
+
+  private void productId;
+  private void productDescription;
+  
+  //
+  // Constructors
+  //
+  public SUPLIER () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of productId
+   * @param newVar the new value of productId
+   */
+  private void setProductId (void newVar) {
+    productId = newVar;
+  }
+
+  /**
+   * Get the value of productId
+   * @return the value of productId
+   */
+  private void getProductId () {
+    return productId;
+  }
+
+  /**
+   * Set the value of productDescription
+   * @param newVar the new value of productDescription
+   */
+  private void setProductDescription (void newVar) {
+    productDescription = newVar;
+  }
+
+  /**
+   * Get the value of productDescription
+   * @return the value of productDescription
+   */
+  private void getProductDescription () {
+    return productDescription;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void receivePayment()
+  {
+  }
+
+
+  /**
+   */
+  public void cancelPayment()
+  {
+  }
+
+
+}
---
+++ EX-12 FOREIGN TRADING MANAGEMENT SYSTEM/CODE/TRADING_MANAGEMENT_SYSTEM.java
@@ -0,0 +1,103 @@
+
+
+/**
+ * Class TRADING_MANAGEMENT_SYSTEM
+ */
+public class TRADING_MANAGEMENT_SYSTEM {
+
+  //
+  // Fields
+  //
+
+  private String verify_product;
+  private int amount;
+  private String product_description;
+  
+  //
+  // Constructors
+  //
+  public TRADING_MANAGEMENT_SYSTEM () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verify_product
+   * @param newVar the new value of verify_product
+   */
+  private void setVerify_product (String newVar) {
+    verify_product = newVar;
+  }
+
+  /**
+   * Get the value of verify_product
+   * @return the value of verify_product
+   */
+  private String getVerify_product () {
+    return verify_product;
+  }
+
+  /**
+   * Set the value of amount
+   * @param newVar the new value of amount
+   */
+  private void setAmount (int newVar) {
+    amount = newVar;
+  }
+
+  /**
+   * Get the value of amount
+   * @return the value of amount
+   */
+  private int getAmount () {
+    return amount;
+  }
+
+  /**
+   * Set the value of product_description
+   * @param newVar the new value of product_description
+   */
+  private void setProduct_description (String newVar) {
+    product_description = newVar;
+  }
+
+  /**
+   * Get the value of product_description
+   * @return the value of product_description
+   */
+  private String getProduct_description () {
+    return product_description;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void trasnport()
+  {
+  }
+
+
+  /**
+   */
+  public void deliver()
+  {
+  }
+
+
+  /**
+   */
+  public void money_transfer()
+  {
+  }
+
+
+}
