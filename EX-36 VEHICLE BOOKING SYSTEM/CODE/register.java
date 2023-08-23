0bdad4e7266afd0c4b96467259e2733944f0ddd1
barathkumar-p <81065407+barathkumar-p@users.noreply.github.com>
2023-08-23 09:36:30 +0530

    Add files via upload

---
+++ EX-36 VEHICLE BOOKING SYSTEM/CODE/register.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class register
+ */
+public class register {
+
+  //
+  // Fields
+  //
+
+  private void registeration;
+  private void data_storage;
+  
+  //
+  // Constructors
+  //
+  public register () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of registeration
+   * @param newVar the new value of registeration
+   */
+  private void setRegisteration (void newVar) {
+    registeration = newVar;
+  }
+
+  /**
+   * Get the value of registeration
+   * @return the value of registeration
+   */
+  private void getRegisteration () {
+    return registeration;
+  }
+
+  /**
+   * Set the value of data_storage
+   * @param newVar the new value of data_storage
+   */
+  private void setData_storage (void newVar) {
+    data_storage = newVar;
+  }
+
+  /**
+   * Get the value of data_storage
+   * @return the value of data_storage
+   */
+  private void getData_storage () {
+    return data_storage;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void add_()
+  {
+  }
+
+
+  /**
+   */
+  public void update()
+  {
+  }
+
+
+}
